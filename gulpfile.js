b37331c9fdce2d86b8b8f9a6c7d2cba44741d543
Noski <hante_hunter@163.com>
2016-11-28 21:51:47 +0800

    完成部分配置

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./source/scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./source/css'));
+});
+
+gulp.task('default', function () {
+  gulp.run(['sass']);
+  gulp.watch('./source/scss/**/*.scss', ['sass']);
+});
